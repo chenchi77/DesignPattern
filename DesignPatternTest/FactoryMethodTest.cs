9224faf51d51f92290f908cfe07adb708dbfaaff
Charlie Chen <yukutou@gmail.com>
2017-09-29 15:31:29 +0800

    test

---
+++ DesignPatternTest/FactoryMethodTest.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.FactoryMethod;
+using NUnit.Framework;
+
+namespace DesignPatternTest
+{
+    [TestFixture]
+    public class FactoryMethodTest
+    {
+        [Test]
+        public void WhenUseTpBankWillReturnTpBankMethod()
+        {
+            var bankFactory = new TpBankFactory();
+            var bank = bankFactory.CreateBank();
+            Assert.AreEqual("TP Deposite", bank.Deposite());
+            Assert.AreEqual("TP Withdraw", bank.Withdraw());
+        }
+
+		[Test]
+		public void WhenUseTnBankWillReturnTnBankMethod()
+		{
+            var bankFactory = new TnBankFactory();
+			var bank = bankFactory.CreateBank();
+			Assert.AreEqual("TN Deposite", bank.Deposite());
+			Assert.AreEqual("TN Withdraw", bank.Withdraw());
+		}
+    }
+}
