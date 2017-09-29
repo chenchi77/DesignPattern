281beed6cfdad4e5525990367369edc931d13754
Charlie Chen <yukutou@gmail.com>
2017-09-29 10:50:42 +0800

    test

---
+++ DesignPatternTest/DecoratorTest.cs
@@ -0,0 +1,18 @@
+﻿using DesignPattern.Decorator;
+using NUnit.Framework;
+
+namespace DesignPatternTest
+{
+    [TestFixture]
+    public class DecoratorTest
+    {
+        [Test]
+        public void WhenUseDecoratorWillReturnDecoratorShortAndTShirt()
+        {
+			var person = new Person("Xiao Ming");
+			var sh = new DecorationShort(person);
+			var ts = new DecorationTShirt(sh);
+			Assert.AreEqual("My name is Xiao Ming Decorator Short Decorator TShirt", ts.Show());
+		}
+    }
+}
