6a11fe24216dd4b74e70b4f840ea0128953adc0c
Charlie Chen <yukutou@gmail.com>
2017-09-29 14:23:18 +0800

    test

---
+++ DesignPatternTest/ProxyTest.cs
@@ -0,0 +1,16 @@
+﻿using DesignPattern.Proxy;
+using NUnit.Framework;
+
+namespace DesignPatternTest
+{
+    [TestFixture]
+    public class ProxyTest
+    {
+        [Test]
+        public void WhenProxyImageWillReturnProxyResult()
+        {
+            var image = new ProxyImage("Picture.jpg");
+            Assert.AreEqual("Displaying Picture.jpg", image.Display());
+        }
+    }
+}
